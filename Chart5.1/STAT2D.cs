2fc4c212877f0ad16f1880f0e776610dfa268231
Vladislav Kovdrya <kovdryavlad@gmail.com>
2018-11-09 00:32:39 +0200

    solve using problems

--- Chart5.1/STAT2D.cs
+++ Chart5.1/STAT2D.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms.DataVisualization.Charting;
 using Disrtibutions;
-using SetsOpearations;
 using System.Drawing;
 using Chart5._1;
 
